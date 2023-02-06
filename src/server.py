90206bf2f3d74f6af9f16c4f4b7e684b10a48d81
august-tengland <54542533+august-tengland@users.noreply.github.com>
2023-02-06 15:40:19 +0100

    feature: Added Github Webhook Reciever. Fixes #2

--- src/server.py
+++ src/server.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from github_webhook import Webhook
 
 app = Flask(__name__)
+webhook = Webhook(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@webhook.hook()
+def on_push(data):
+    print("Got push with: {0}".format(data))
 
 if __name__ == "__main__":
     app.run(port=8080)
