e93a759d813ee3a12c00c2d8b806d1f41e6b3718
Erik Hedlund <erikcghedlund@outlook.com>
2023-02-08 13:26:27 +0100

    remove comments that should be there, relates to #24

--- src/server.py
+++ src/server.py
@@ -21,6 +21,5 @@ def webhook():
             return make_response("fail", 400)
 
 if __name__ == "__main__":
-    print(run_tests())
-    # print(public_url)
-    # app.run(port=8080)
+    print(public_url)
+    app.run(port=8080)
